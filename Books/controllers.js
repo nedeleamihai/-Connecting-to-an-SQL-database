3f35513b3c81dfa86380cf4b3950b5ff3e72b763
Nedelea Mihai <nedeleamihai11@gmail.com>
2020-06-12 17:14:24 +0100

    Add files via upload

---
+++ Books/controllers.js
@@ -0,0 +1,134 @@
+const express = require('express');
+
+const BooksService = require('./services.js');
+
+const {
+    validateFields
+} = require('../utils');
+
+const {
+    ServerError
+} = require('../errors');
+
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+    const {
+        name,
+        author_id
+    } = req.body;
+
+    // validare de campuri
+    try {
+
+        const fieldsToBeValidated = {
+            name: {
+                value: name,
+                type: 'alpha'
+            },
+            author_id: {
+                value: author_id,
+                type: 'int'
+            }
+        };
+
+        validateFields(fieldsToBeValidated);
+
+        await BooksService.add(name, author_id);
+
+        res.status(201).end();
+    } catch (err) {
+        // daca primesc eroare, pasez eroarea mai departe la handler-ul de errori declarat ca middleware in start.js 
+        next(err);
+    }
+});
+
+router.get('/', async (req, res, next) => {
+    try {
+
+        const books = await BooksService.getAll();
+        res.json(books);
+    } catch (err) {
+        // daca primesc eroare, pasez eroarea mai departe la handler-ul de errori declarat ca middleware in start.js 
+        next(err);
+    }
+});
+
+router.get('/:id', async (req, res, next) => {
+    const {
+        id
+    } = req.params;
+    try {
+
+        validateFields({
+            id: {
+                value: id,
+                type: 'int'
+            }
+        });
+        const book = await BooksService.getById(parseInt(id));
+        res.json(book);
+    } catch (err) {
+        // daca primesc eroare, pasez eroarea mai departe la handler-ul de errori declarat ca middleware in start.js 
+        next(err);
+    }
+});
+
+router.put('/:id', async (req, res, next) => {
+    const {
+        id
+    } = req.params;
+    const {
+        name,
+        author_id
+    } = req.body;
+    try {
+
+        const fieldsToBeValidated = {
+            id: {
+                value: id,
+                type: 'int'
+            },
+            name: {
+                value: name,
+                type: 'alpha'
+            },
+            author_id: {
+                value: author_id,
+                type: 'int'
+            }
+        };
+
+        validateFields(fieldsToBeValidated);
+
+        await BooksService.updateById(parseInt(id), name, author_id);
+        res.status(204).end();
+    } catch (err) {
+        // daca primesc eroare, pasez eroarea mai departe la handler-ul de errori declarat ca middleware in start.js 
+        next(err);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    const {
+        id
+    } = req.params;
+
+    try {
+
+        validateFields({
+            id: {
+                value: id,
+                type: 'int'
+            }
+        });
+        // se poate modifica 
+        await BooksService.deleteById(parseInt(id));
+        res.status(204).end();
+    } catch (err) {
+        // daca primesc eroare, pasez eroarea mai departe la handler-ul de errori declarat ca middleware in start.js 
+        next(err);
+    }
+});
+
+module.exports = router;
